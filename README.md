2db649e5dfe39cde927bd4dd3454a1552ab8888f
Steven Quan <63191202+wizardap@users.noreply.github.com>
2023-04-26 10:49:20 +0700

    update readme.md

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Make for my final project in 2023
 ### Tutorial Guide version 0.2
  - Step 1:Extract the file zip
  
- - Step 2: Click on the main.exe
+ - Step 2: Open the terminal
+ - Step 3: Type "make all" and enter
+ - Step 4: Type "make run" to play
+
  
   
--- Sudoku.cpp
+++ Sudoku.cpp
@@ -2,96 +2,101 @@
 
     Basic Sudoku Generator has written in C++
     Writer : WizardAP
-
 */
 
 #include "Sudoku.h"
 Sudoku::Sudoku()
 {
     // Initialize the grid
-    for (int row =0; row<NUM_ROWS; row++)
-        for (int col=0 ; col<NUM_COLS; col++)
+    for (int row = 0; row < NUM_ROWS; row++)
+        for (int col = 0; col < NUM_COLS; col++)
             this->grid[row][col] = UNASSIGNED;
     SUCCESS_FILLED = false;
 }
 int Seed(int maxLimit)
 {
     mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
-    return rng()%maxLimit;
+    return rng() % maxLimit;
 }
 void Sudoku::delay(int milliseconds)
 {
     Sleep(milliseconds);
 }
-bool Sudoku::usedInRow(const int &value,int row)
+bool Sudoku::usedInRow(const int &value, int row)
 {
-    for (int col = 0; col<NUM_COLS; col++)
+    for (int col = 0; col < NUM_COLS; col++)
     {
-        if (this->grid[row][col] == value) return true;
+        if (this->grid[row][col] == value)
+            return true;
     }
     return false;
 }
-bool Sudoku::usedInColumn(const int &value,int col)
+bool Sudoku::usedInColumn(const int &value, int col)
 {
-    for (int row = 0; row<NUM_ROWS; row++)
+    for (int row = 0; row < NUM_ROWS; row++)
     {
-        if (this->grid[row][col] == value) return true;
+        if (this->grid[row][col] == value)
+            return true;
     }
     return false;
 }
-bool Sudoku::usedInBox(const int &value,int boxStartRow,int boxStartCol)
+bool Sudoku::usedInBox(const int &value, int boxStartRow, int boxStartCol)
 {
-    for (int row =0; row<3; row++)
-        for (int col =0; col<3; col++)
+    for (int row = 0; row < 3; row++)
+        for (int col = 0; col < 3; col++)
         {
             int X = boxStartRow + row;
             int Y = boxStartCol + col;
-            if (this->grid[X][Y] == value) return true;
+            if (this->grid[X][Y] == value)
+                return true;
         }
     return false;
 }
-bool Sudoku::isSafe(int value,int row,int col)
+bool Sudoku::isSafe(int value, int row, int col)
 {
-    return !usedInRow(value,row) &&
-           !usedInColumn(value,col) &&
-           !usedInBox(value,row - row%3,col - col%3);
+    return !usedInRow(value, row) &&
+           !usedInColumn(value, col) &&
+           !usedInBox(value, row - row % 3, col - col % 3);
 }
-bool Sudoku::FindEmptyCoordinate(int &row,int &col)
+bool Sudoku::FindEmptyCoordinate(int &row, int &col)
 {
-    for (row=0; row<NUM_ROWS; row++)
-        for (col=0; col<NUM_COLS; col++)
-            if (this->grid[row][col] == 0) return true;
+    for (row = 0; row < NUM_ROWS; row++)
+        for (col = 0; col < NUM_COLS; col++)
+            if (this->grid[row][col] == 0)
+                return true;
     return false;
 }
-int Sudoku::get(int x,int y)
+int Sudoku::get(int x, int y)
 {
     return this->grid[x][y];
 }
-void Sudoku::fillDiagionalBox(int boxStartRow,int boxStartCol)
+void Sudoku::fillDiagionalBox(int boxStartRow, int boxStartCol)
 {
-    int random_list[9] = {1,2,3,4,5,6,7,8,9};
-    random_shuffle(random_list,random_list+9,Seed);
-    for (int row = 0; row<3; row++)
-        for (int col =0; col<3; col++)
+    int random_list[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    random_shuffle(random_list, random_list + 9, Seed);
+    for (int row = 0; row < 3; row++)
+        for (int col = 0; col < 3; col++)
         {
             int X = boxStartRow + row;
             int Y = boxStartCol + col;
-            this->grid[X][Y] =random_list[row*3 + col];
+            this->grid[X][Y] = random_list[row * 3 + col];
         }
 }
 bool Sudoku::fillRemainingBox()
 {
-    int row,col;
-    if (!FindEmptyCoordinate(row,col)) return true;
-    int random_list[9] = {1,2,3,4,5,6,7,8,9};
-    random_shuffle(random_list,random_list+9,Seed);
-    for (int index = 0; index<9; index++)
+    int row, col;
+    if (!FindEmptyCoordinate(row, col))
+        return true;
+    int random_list[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    random_shuffle(random_list, random_list + 9, Seed);
+    for (int index = 0; index < 9; index++)
     {
         int number = random_list[index];
-        if (isSafe(number,row,col))
+        if (isSafe(number, row, col))
         {
             this->grid[row][col] = number;
-            if (fillRemainingBox()) return true;
+            if (fillRemainingBox())
+                return true;
             this->grid[row][col] = UNASSIGNED;
         }
     }
@@ -99,46 +104,49 @@ bool Sudoku::fillRemainingBox()
 }
 void Sudoku::fillPuzzle()
 {
-    for (int coor = 0; coor<9; coor+=3)
-        fillDiagionalBox(coor,coor);
-    if (fillRemainingBox()) SUCCESS_FILLED = true;
+    for (int coor = 0; coor < 9; coor += 3)
+        fillDiagionalBox(coor, coor);
+    if (fillRemainingBox())
+        SUCCESS_FILLED = true;
     if (!SUCCESS_FILLED)
     {
-        cout<<"This generator code is error ! Please fix this as soon asp possible!";
+        cout << "This generator code is error ! Please fix this as soon asp possible!";
         exit(1);
     }
 }
 int Sudoku::countSolutions()
 {
-    int row,col;
-    if (!FindEmptyCoordinate(row,col)) return 1;
-    int solutions =0;
-    for (int number = 1; number<=9; number++)
-        if (isSafe(number,row,col))
+    int row, col;
+    if (!FindEmptyCoordinate(row, col))
+        return 1;
+    int solutions = 0;
+    for (int number = 1; number <= 9; number++)
+        if (isSafe(number, row, col))
         {
             this->grid[row][col] = number;
-            solutions+=countSolutions();
+            solutions += countSolutions();
             this->grid[row][col] = UNASSIGNED;
-            if (solutions>1) return solutions;
+            if (solutions > 1)
+                return solutions;
         }
     return solutions;
-
 }
-void Sudoku::gotoXY(int X,int Y)
+void Sudoku::gotoXY(int X, int Y)
 {
     COORD screen;
     static const HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
     cout.flush();
     screen.X = X;
     screen.Y = Y;
-    SetConsoleCursorPosition(output,screen);
+    SetConsoleCursorPosition(output, screen);
 }
 bool Sudoku::solvePuzzle(bool simulation)
 {
-    int row,col;
-    if (!FindEmptyCoordinate(row,col)) return true;
-    for (int number =1; number<=9; number++)
-        if (isSafe(number,row,col))
+    int row, col;
+    if (!FindEmptyCoordinate(row, col))
+        return true;
+    for (int number = 1; number <= 9; number++)
+        if (isSafe(number, row, col))
         {
             this->grid[row][col] = number;
             if (simulation)
@@ -146,7 +154,8 @@ bool Sudoku::solvePuzzle(bool simulation)
                 delay(TIME_DELAYED);
                 printPuzzle();
             }
-            if (solvePuzzle(simulation)) return true;
+            if (solvePuzzle(simulation))
+                return true;
             this->grid[row][col] = UNASSIGNED;
             if (simulation)
             {
@@ -158,50 +167,51 @@ bool Sudoku::solvePuzzle(bool simulation)
 }
 void Sudoku::generatePuzzle(int difficulty)
 {
-    for (int row = 0; row<NUM_ROWS; row++)
-        for (int col =0; col<NUM_COLS; col++)
+    for (int row = 0; row < NUM_ROWS; row++)
+        for (int col = 0; col < NUM_COLS; col++)
             this->grid[row][col] = UNASSIGNED;
     fillPuzzle();
 
-    for (int row = 0 ;row<NUM_ROWS;row++)
-        for (int col = 0;col<NUM_COLS;col++)
+    for (int row = 0; row < NUM_ROWS; row++)
+        for (int col = 0; col < NUM_COLS; col++)
             this->answer_grid[row][col] = this->grid[row][col];
 
-    while(difficulty >0)
+    while (difficulty > 0)
     {
-        int row,col;
+        int row, col;
         do
         {
             row = Seed(8);
             col = Seed(8);
-        }
-        while(this->grid[row][col] ==UNASSIGNED);
+        } while (this->grid[row][col] == UNASSIGNED);
         int tempValue = this->grid[row][col];
         this->grid[row][col] = UNASSIGNED;
-        if (countSolutions()!=1)
+        if (countSolutions() != 1)
         {
             this->grid[row][col] = tempValue;
         }
-        else difficulty--;
+        else
+            difficulty--;
     }
 }
 bool Sudoku::checkSolution()
 {
-    for (int row =0; row<NUM_ROWS; row++)
+    for (int row = 0; row < NUM_ROWS; row++)
     {
-        bool freq[10]= {false};
-        for (int col= 0; col<NUM_COLS; col++)
+        bool freq[10] = {false};
+        for (int col = 0; col < NUM_COLS; col++)
         {
             int value = this->grid[row][col];
-            if (freq[value] == true) return false;
+            if (freq[value] == true)
+                return false;
             freq[value] = true;
         }
     }
 
-    for (int col=0; col<NUM_COLS; col++)
+    for (int col = 0; col < NUM_COLS; col++)
     {
         bool freq[10] = {false};
-        for (int row = 0; row<NUM_ROWS; row++)
+        for (int row = 0; row < NUM_ROWS; row++)
         {
             int value = this->grid[row][col];
             if (freq[value] == true)
@@ -212,14 +222,14 @@ bool Sudoku::checkSolution()
         }
     }
 
-    for (int boxStartRow = 0; boxStartRow<9; boxStartRow+=3)
-        for (int boxStartCol = 0; boxStartCol<9; boxStartCol+=3)
+    for (int boxStartRow = 0; boxStartRow < 9; boxStartRow += 3)
+        for (int boxStartCol = 0; boxStartCol < 9; boxStartCol += 3)
         {
-            bool freq[10]= {false};
-            for (int row = 0; row<3; row++)
-                for (int col =0; col<3; col++)
+            bool freq[10] = {false};
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
                 {
-                    int X = boxStartRow+ row;
+                    int X = boxStartRow + row;
                     int Y = boxStartCol + col;
                     int value = this->grid[X][Y];
                     if (freq[value] == true)
@@ -228,7 +238,6 @@ bool Sudoku::checkSolution()
                     }
                     freq[value] = true;
                 }
-
         }
 
     return true;
@@ -237,35 +246,39 @@ void Sudoku::printPuzzle()
 {
     clearConsole();
     int numberOfUnderlines = 77;
-    for (int i = 0; i<numberOfUnderlines; i++)
-        cout<<"=";
-    cout<<endl;
-    for (int row =0; row<NUM_ROWS; row++)
+    for (int i = 0; i < numberOfUnderlines; i++)
+        cout << "=";
+    cout << endl;
+    for (int row = 0; row < NUM_ROWS; row++)
     {
-        for (int rowSpaceLines = 0; rowSpaceLines<3; rowSpaceLines++)
+        for (int rowSpaceLines = 0; rowSpaceLines < 3; rowSpaceLines++)
         {
-            for (int col = 0; col<NUM_COLS; col++)
+            for (int col = 0; col < NUM_COLS; col++)
             {
-                if (col % 3==0) cout<<'|';
-                cout<<'|';
-                for (int spaces = 0; spaces<7; spaces++)
-                    if (spaces == 3 && rowSpaceLines ==1)
+                if (col % 3 == 0)
+                    cout << '|';
+                cout << '|';
+                for (int spaces = 0; spaces < 7; spaces++)
+                    if (spaces == 3 && rowSpaceLines == 1)
                     {
-                        if (this->grid[row][col] ==  UNASSIGNED)
+                        if (this->grid[row][col] == UNASSIGNED)
                         {
-                            cout<<' ';
+                            cout << ' ';
                         }
-                        else cout<<this->grid[row][col];
+                        else
+                            cout << this->grid[row][col];
                     }
                     else
-                        cout<<' ';
+                        cout << ' ';
             }
-            cout<<"||\n";
+            cout << "||\n";
         }
-        for (int underline =0; underline<numberOfUnderlines; underline++)
-            if ((row+1) % 3==0 ) cout<<"=";
-            else cout<<"-";
-        cout<<endl;
+        for (int underline = 0; underline < numberOfUnderlines; underline++)
+            if ((row + 1) % 3 == 0)
+                cout << "=";
+            else
+                cout << "-";
+        cout << endl;
         // cout.flush();
     }
 }
@@ -273,9 +286,9 @@ void Sudoku::clearConsole()
 {
     system("cls");
 }
-bool Sudoku::changeValue(int row,int col,int number)
+bool Sudoku::changeValue(int row, int col, int number)
 {
-    if (isSafe(number,row,col))
+    if (isSafe(number, row, col))
     {
         this->grid[row][col] = number;
         return true;
@@ -284,43 +297,48 @@ bool Sudoku::changeValue(int row,int col,int number)
 }
 bool Sudoku::isFull()
 {
-    for (int row =0;row<NUM_ROWS;row++)
-        for (int col =0;col<NUM_COLS;col++)
-        if (this->grid[row][col] == UNASSIGNED) return false;
+    for (int row = 0; row < NUM_ROWS; row++)
+        for (int col = 0; col < NUM_COLS; col++)
+            if (this->grid[row][col] == UNASSIGNED)
+                return false;
     return true;
 }
 void Sudoku::printAnswerPuzzle()
 {
     clearConsole();
     int numberOfUnderlines = 77;
-    for (int i = 0; i<numberOfUnderlines; i++)
-        cout<<"=";
-    cout<<endl;
-    for (int row =0; row<NUM_ROWS; row++)
+    for (int i = 0; i < numberOfUnderlines; i++)
+        cout << "=";
+    cout << endl;
+    for (int row = 0; row < NUM_ROWS; row++)
     {
-        for (int rowSpaceLines = 0; rowSpaceLines<3; rowSpaceLines++)
+        for (int rowSpaceLines = 0; rowSpaceLines < 3; rowSpaceLines++)
         {
-            for (int col = 0; col<NUM_COLS; col++)
+            for (int col = 0; col < NUM_COLS; col++)
             {
-                if (col % 3==0) cout<<'|';
-                cout<<'|';
-                for (int spaces = 0; spaces<7; spaces++)
-                    if (spaces == 3 && rowSpaceLines ==1)
+                if (col % 3 == 0)
+                    cout << '|';
+                cout << '|';
+                for (int spaces = 0; spaces < 7; spaces++)
+                    if (spaces == 3 && rowSpaceLines == 1)
                     {
-                        if (this->answer_grid[row][col] ==  UNASSIGNED)
+                        if (this->answer_grid[row][col] == UNASSIGNED)
                         {
-                            cout<<' ';
+                            cout << ' ';
                         }
-                        else cout<<this->answer_grid[row][col];
+                        else
+                            cout << this->answer_grid[row][col];
                     }
                     else
-                        cout<<' ';
+                        cout << ' ';
             }
-            cout<<"||\n";
+            cout << "||\n";
         }
-        for (int underline =0; underline<numberOfUnderlines; underline++)
-            if ((row+1) % 3==0 ) cout<<"=";
-            else cout<<"-";
-        cout<<endl;
+        for (int underline = 0; underline < numberOfUnderlines; underline++)
+            if ((row + 1) % 3 == 0)
+                cout << "=";
+            else
+                cout << "-";
+        cout << endl;
     }
 }
